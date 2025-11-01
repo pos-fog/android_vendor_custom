968f34208e44e773d8b11fe98e5938d3e721d748
alternoegraha <noegrahachan@gmail.com>
2025-11-01 09:09:09 +0000

    config: enable OTA updater for unofficial builds
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- config/ota.mk
+++ config/ota.mk
@@ -1,9 +1,7 @@
 # Updater
-ifeq ($(IS_OFFICIAL),true)
 PRODUCT_PACKAGES += \
     Updater
 
 PRODUCT_COPY_FILES += \
     vendor/custom/config/permissions/privapp-permissions-custom.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-custom.xml
 
-endif
